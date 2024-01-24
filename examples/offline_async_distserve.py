a5d710bce3903b4115dec2c5784a5a0e808a8073
Ginda Chen <32371474+GindaChen@users.noreply.github.com>
2024-01-23 23:16:56 -0800

    Add user level logging

--- examples/offline_async_distserve.py
+++ examples/offline_async_distserve.py
@@ -32,7 +32,7 @@ async def main():
     final_output = []
     async for request_output in results_generator:
         final_output.append(request_output)
-        print(request_output)
+        print(f"[User] At user level, received output for request: {request_output = }")
 
     return final_output
 
