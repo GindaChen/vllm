d30d46d2ecf99b5f0fd6cfc7457b289d2d34410b
Ginda Chen <32371474+GindaChen@users.noreply.github.com>
2024-03-12 01:18:34 -0700

    Force eager execution

--- examples/bench_interference.py
+++ examples/bench_interference.py
@@ -1,16 +1,31 @@
 # Start a vLLM server
-from vllm import LLM, SamplingParams, AsyncLLMEngine
+from vllm import LLM, SamplingParams, AsyncLLMEngine, LLMEngine
+from vllm.worker.worker import Worker
+from vllm.executor.gpu_executor import GPUExecutor
 
 llm = LLM(
     model="facebook/opt-125m",
     gpu_memory_utilization=0.5,
+    # disable graph caching
+    enforce_eager=True,
+
 )
 prompts = [
     "a a a a",
+    "a a a a a a a",
 ]
 sampling_params = SamplingParams(temperature=0, top_p=1)
 
-engine = llm.llm_engine
+engine: 'LLMEngine' = llm.llm_engine
+model_executor = engine.model_executor
+assert isinstance(model_executor, GPUExecutor)
+
+worker = model_executor.driver_worker
+assert isinstance(worker, Worker)
+
+model_runner = worker.model_runner
+model_runner._prepare_prompt
+model_runner._prepare_decode
 
 # This just add a few requests into the LLM engine.
 llm.generate(prompts, sampling_params, should_run=False)
